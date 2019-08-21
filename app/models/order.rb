9e196d2008b083b65c53fca0362cd9c87d222428
Henry Neal <henry.neal0@gmail.com>
2019-08-21 16:25:13 -0400

    step two done

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,8 @@
 class Order < ApplicationRecord
   has_many :order_products
-  has_many :proudcts through :order_products
-  belongs_to :customers
-  def products
-    product_ids = OrderProduct.where(order_id: id).pluck(:product_id)
-    Product.find(product_ids)
-  end
-
+  has_many :products through :order_products #creates method called products
+  belongs_to :customer
+  
   def shippable?
     status != "shipped" && products.count >= 1
   end
